45fd3182261b15ac436de60364f38e4d7d8abe2f
Leonardo <dominguezleonardo26@gmail.com>
2024-02-28 14:04:23 -0300

    Finalización del proyecto

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs/promises");
+const Excel = require('excel4node');
+
 
 (async () => {
   let browser;
 
   try {
-    browser = await puppeteer.launch({headless: false,});
+    browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
+    
     const codigos = await fs.readFile("expedientes.txt", "utf-8");
     const codigosArray = codigos
-      .split("\n")
-      .filter((linea) => linea.trim() !== "");
+    .split("\n")
+    .filter((linea) => linea.trim() !== "");
     const codigosLimpios = codigosArray.map((codigo) => codigo.trim());
-    const objeto = {};
+    let objeto = {};
+    try {
+      const jsonData = await fs.readFile('resultados.json', 'utf-8');
+      objeto = JSON.parse(jsonData);
+    } catch (error) {
+      console.log('No se encontraron datos previos en el archivo JSON. Se crearán nuevos datos.');
+      
+    }
+  
 
     for (const codigo of codigosLimpios) {
       await page.goto(
         "https://www.anses.gob.ar/consultas/consulta-de-expediente"
-      );
-      await new Promise((resolve) => setTimeout(resolve, 5000));
+
+      );    
 
       try {
         await page.waitForSelector("#edit-nro-expediente", { timeout: 5000 });
@@ -34,12 +45,26 @@ const fs = require("fs/promises");
 
       await new Promise((resolve) => setTimeout(resolve, 5000));
 
+     
+
       try {
         await page.waitForSelector("[class=container]", { timeout: 5000 });
         const small = await page.$eval(
           "div.constancia-inner small",
           (element) => element.textContent.trim()
         );
+
+        const fecha = await page.$eval(
+          "div.fechas h4",
+          (element) => element.textContent.trim()
+        );
+         
+        const nombreCompleto = await page.evaluate(() => {
+          const h2Element = document.querySelector(".constancia-inner h2");
+          const nombre = h2Element.childNodes[0].nodeValue.trim();
+          return nombre;
+        });
+
         const dni = small.split(" ")[2];
         const estadoRespuesta = await page.$eval("div.estado h4", (element) =>
           element.textContent.trim()
@@ -59,23 +84,43 @@ const fs = require("fs/promises");
           estadoRespuesta.startsWith("Tu trámite se encontraba mal caratulado")
         ) {
           estado = "Cambio de carátula";
-        } else if (estadoRespuesta.startsWith("Es necesario que aportes documentación")){
+        } else if (estadoRespuesta.startsWith("Es necesario que aportes documentación")) {
           estado = "Iniciado / Requiere documentación"
         }
-         else {
+        else {
           estado = "Iniciado";
         }
 
         if (objeto[dni]) {
-          objeto[dni] = { ...objeto[dni], [codigo]: estado };
+          objeto[dni] = { ...objeto[dni], Expediente: codigo, nombre: nombreCompleto, Estado: estado, ultima_Revisión: fecha };
         } else {
-          objeto[dni] = { [codigo]: estado };
+          objeto[dni] = { Expediente: codigo, Nombre: nombreCompleto, Estado: estado, "Ultima Revisión": fecha };
         }
       } catch (error) {
         console.error(` este Expediente no tiene tramite iniciado ${codigo} `);
       }
     }
-    await fs.writeFile("resultados.json", JSON.stringify(objeto, null));
+    await fs.writeFile("resultados.json", JSON.stringify(objeto, null, 2));
+    const jsonData = await fs.readFile('resultados.json', 'utf-8');
+    const data = JSON.parse(jsonData);
+    //crear libro excel
+    const workbook = new Excel.Workbook();
+    const worksheet = workbook.addWorksheet("Datos");
+
+    const headers = Object.keys(objeto[Object.keys(objeto)[0]]);
+    headers.forEach((header, index) => {
+      worksheet.cell(1, index + 1).string(header);
+    });
+
+    Object.values(objeto).forEach((row, rowIndex) => {
+      headers.forEach((header, colIndex) => {
+        worksheet.cell(rowIndex + 2, colIndex + 1).string(String(row[header]));
+      });
+    });
+
+    await workbook.write("datos.xlsx");
+    console.log("Archivo Excel guardado correctamente.");
+
   } catch (error) {
     console.error("Error:", error);
   } finally {
